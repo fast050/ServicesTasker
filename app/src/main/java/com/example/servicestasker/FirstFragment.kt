76a21a272e143dfc96b887b442d6caf38683849c
fast050 <dragonman050@gmail.com>
2022-04-16 16:01:25 +0400

    -start with service : add service , start service , stop service

---
+++ app/src/main/java/com/example/servicestasker/FirstFragment.kt
@@ -0,0 +1,62 @@
+package com.example.servicestasker
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.servicestasker.databinding.FragmentFirstBinding
+import com.example.servicestasker.service.MyService
+
+/**
+ * A simple [Fragment] subclass as the default destination in the navigation.
+ */
+class FirstFragment : Fragment() {
+
+    private var _binding: FragmentFirstBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+        binding.buttonFirst.setOnClickListener {
+            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        }
+
+
+        binding.apply {
+
+            val intent = Intent(requireContext(),MyService::class.java)
+
+            btnStartService.setOnClickListener{
+              activity?.startService(intent)
+            }
+
+            btnStopService.setOnClickListener {
+                activity?.stopService(intent)
+            }
+        }
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
