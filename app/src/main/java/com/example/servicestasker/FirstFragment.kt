08424102ffa005a9413005e935c9692680e23d00
fast050 <dragonman050@gmail.com>
2022-04-17 02:17:52 +0400

    - Bound Service Local Bound

--- app/src/main/java/com/example/servicestasker/FirstFragment.kt
+++ app/src/main/java/com/example/servicestasker/FirstFragment.kt
@@ -1,20 +1,30 @@
 package com.example.servicestasker
 
+import android.content.ComponentName
+import android.content.Context
 import android.content.Intent
+import android.content.ServiceConnection
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.os.IBinder
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.servicestasker.databinding.FragmentFirstBinding
 import com.example.servicestasker.service.MyService
+import com.example.servicestasker.service.MyService.MyServiceBinder
+
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class FirstFragment : Fragment() {
 
+    private var myService :MyService? = null
+    private var isServiceBound:Boolean = false
+    private lateinit var serviceConnection: ServiceConnection
     private var _binding: FragmentFirstBinding? = null
 
     // This property is only valid between onCreateView and
@@ -39,17 +49,57 @@ class FirstFragment : Fragment() {
             findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
         }
 
+        val serviceIntent = Intent(requireContext(),MyService::class.java)
+
 
         binding.apply {
 
-            val intent = Intent(requireContext(),MyService::class.java)
 
             btnStartService.setOnClickListener{
-              activity?.startService(intent)
+              activity?.startService(serviceIntent)
             }
 
             btnStopService.setOnClickListener {
-                activity?.stopService(intent)
+                activity?.stopService(serviceIntent)
+            }
+
+            btnBindService.setOnClickListener {
+
+                if (myService == null){
+                    serviceConnection = object : ServiceConnection{
+                        override fun onServiceConnected(p0: ComponentName?, p1: IBinder?) {
+
+                            //to get reference from the service
+                            val myServiceBinder = p1 as MyServiceBinder
+                            myService = myServiceBinder.getService()
+                            isServiceBound =true
+                        }
+
+                        override fun onServiceDisconnected(p0: ComponentName?) {
+                           isServiceBound=false
+                        }
+
+                    }
+                }
+
+                // BIND_AUTO_CREATE : it will create service if is not created when it try to bind
+                activity?.bindService(serviceIntent,serviceConnection, Context.BIND_AUTO_CREATE)
+
+
+            }
+
+            btnUnbindService.setOnClickListener {
+                 if (isServiceBound) {
+                     activity?.unbindService(serviceConnection)
+                     isServiceBound = false
+                 }
+            }
+
+            btnGetRandomNumber.setOnClickListener {
+                if (isServiceBound)
+                    textviewFirst.text = myService?.randomNumber.toString()
+                else
+                    textviewFirst.text = "not bound"
             }
         }
 
--- app/src/main/java/com/example/servicestasker/service/MyService.kt
+++ app/src/main/java/com/example/servicestasker/service/MyService.kt
@@ -2,6 +2,7 @@ package com.example.servicestasker.service
 
 import android.app.Service
 import android.content.Intent
+import android.os.Binder
 import android.os.IBinder
 import android.util.Log
 import kotlinx.coroutines.*
@@ -12,12 +13,17 @@ const val TAG = "*class : MyService "
 class MyService: Service() {
 
     private var isGenerationActive =false
-    private val job = Job()
-    var numberLimit = 1  // to break the loop when reach some condition ( numberLimit > 100 )
-    private val coroutineScope = CoroutineScope(job) // custom coroutine
+    private var numberLimit = 1  // to break the loop when reach some condition ( numberLimit > 100 )
+    var randomNumber = 0
 
-    override fun onBind(p0: Intent?): IBinder? {
-     return null
+    // coroutine job
+    private val job = Job()
+    private val coroutineScope = CoroutineScope(job) // custom coroutine
+    // Binder given to clients
+    private val binder = MyServiceBinder()
+
+    override fun onBind(p0: Intent?): IBinder {
+     return binder
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
@@ -26,26 +32,27 @@ class MyService: Service() {
 
         isGenerationActive=true
 
-        coroutineScope.launch {
+        coroutineScope.launch(Dispatchers.IO){
+            yield()
             randomNumberGeneration()   // why don't cancel even i all job cancel
-            delay(0)
         }
 
         return super.onStartCommand(intent, flags, startId)
     }
 
     override fun onDestroy() {
-        super.onDestroy()
         stopRandomNumberGeneration()
         Log.d(TAG, "onDestroy: Service Stopped ")
+        super.onDestroy()
+
     }
 
     private fun randomNumberGeneration()
     {
         while (isGenerationActive){
-            val randomNumber = (1 .. 100).random()
+            randomNumber = (1 .. 100).random()
             Thread.sleep(1000)
-            Log.d(TAG, "startRandomNumberGeneration: current number $randomNumber")
+            Log.d(TAG, "current number $randomNumber | current Thread ${Thread.currentThread()}")
             numberLimit++
 
             if (numberLimit>50)
@@ -54,8 +61,18 @@ class MyService: Service() {
     }
     
     private fun stopRandomNumberGeneration(){
-       // isGenerationActive=false
+        isGenerationActive=false
         job.cancel()
         Log.d(TAG, "stopRandomNumberGeneration: has stop")
+    }
+
+
+    /**
+     * Class used for the client Binder.  Because we know this service always
+     * runs in the same process as its clients, we don't need to deal with IPC.
+     */
+    inner class MyServiceBinder : Binder() {
+        // Return this instance of LocalService so clients can call public methods
+        fun getService(): MyService = this@MyService
     }
 }
